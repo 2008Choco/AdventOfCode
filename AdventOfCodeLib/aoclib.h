9de0f80b431ffb00428618bdfdd043459fafea3b
Parker Hawke <hawkeboyz2@hotmail.com>
2023-12-02 10:21:47 -0500

    Bump aoclib to cpp17, document aoclib::readInput()

--- AdventOfCodeLib/aoclib.h
+++ AdventOfCodeLib/aoclib.h
@@ -3,9 +3,19 @@
 #include <string>
 #include <vector>
 
-#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+#define WIN32_LEAN_AND_MEAN // Exclude rarely-used stuff from Windows headers
 
 namespace aoclib
 {
-    const std::vector<std::string> readInput(const std::string& inputFilePath);
+    /**
+     * Read a newline-delimited file at the given path and return its lines
+     * in a std::vector.
+     *
+     * @param inputFilePath the path to the file from which to read
+     *
+     * @return the file lines
+     *
+     * @throws std::invalid_argument if the file stream failed to open
+     */
+    [[nodiscard]] const std::vector<std::string> readInput(const std::string& inputFilePath);
 }
